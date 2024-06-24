9544f8a5e634e0ea09e5eb3d0bb230135ee7cd6b
Bola <boyetade@hotmail.com>
2024-06-24 17:59:26 +0100

    pages prop

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -2,12 +2,18 @@
 
 import RecipeCard from "./Components/RecipeCard";
 import TitleHeader from "../../../Components/TitleHeader";
+import { useState } from "react";
 
 export default function DiscoverPage({ searchParams }: { searchParams: any }) {
+  const [bookmarkedRecipe, setBookmarkedRecipe] = useState<string[]>([]);
   return (
     <>
       <TitleHeader />
-      <RecipeCard recipeQuery={searchParams.recipeQuery} />
+      <RecipeCard
+        bookmarkedRecipe={bookmarkedRecipe}
+        setBookmarkedRecipe={setBookmarkedRecipe}
+        recipeQuery={searchParams.recipeQuery}
+      />
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import TitleHeader from "../../Components/TitleHeader";
 import RecipeCarousel from "../../Components/RecipeCarousel";
 import ByIngredients from "../../Components/ByIngredients";
 import FeatureRecipes from "../../Components/FeatureRecipes";
+import { useState } from "react";
 
 export default function Home() {
   return (
