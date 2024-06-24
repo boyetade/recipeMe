cab6d8ef0a74b151c3dc26781072b8848fa83318
Bola <boyetade@hotmail.com>
2024-06-24 17:51:20 +0100

    header redirection to home

--- Components/TitleHeader.tsx
+++ Components/TitleHeader.tsx
@@ -1,10 +1,18 @@
 import { inter } from "@/app/font";
 import variables from "../variable.module.scss";
+import Link from "next/link";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import RecipeBookmark from "./BookmarkMenu";
 
 const TitleHeader = () => {
   return (
-    <div className={variables.titleHeader}>
-      <p className={inter.className}>RecipeMe</p>
+    <div className="flex justify-center items-center  p-[70px] tracking-[7px] text-2xl ">
+      <div>
+        <Link href={"/ "}>
+          <p className="justify-center">RecipeMe</p>
+        </Link>{" "}
+      </div>
+      <div></div>
     </div>
   );
 };
